45b73fb0a4133d37f8808fb980e4d2df1582b898
Terry CLombardi <83511726+TerryL1971@users.noreply.github.com>
2025-06-23 00:44:15 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio/', // ✅ Must match your repo name
+  base: '/', 
   plugins: [react()],
 });
